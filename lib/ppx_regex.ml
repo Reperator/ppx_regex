b57b0779411b8e2d59073df395fa4bc896043262
Fabian Bonk <fabian.bonk@bonkii.com>
2016-11-06 18:13:30 +0100

    extract <e> from match%regex <e> with ...

--- lib/ppx_regex.ml
+++ lib/ppx_regex.ml
@@ -1,10 +1,11 @@
 open Parsetree
 open Asttypes
+open Longident
 open Ast_mapper
 open Ppx_core.Std
 open Ast_builder.Default
 
-let accept_cases e cases =
+let accept_cases cases =
   let f case =
     begin match case.pc_guard with
     | None -> ()
@@ -19,8 +20,18 @@ let accept_cases e cases =
       let guard =
         let nl v = (Nolabel, v) in
         let regex =
-          pexp_apply ~loc:ppat_loc [%expr Re_str.regexp] [nl (pexp_constant ~loc:ppat_loc str)] in
-        pexp_apply ~loc:ppat_loc [%expr Re_str.string_match] [nl regex; nl e; nl [%expr 0]] in
+          pexp_apply
+            ~loc:ppat_loc
+            [%expr Re_str.regexp]
+            [nl (pexp_constant ~loc:ppat_loc str)] in
+        let ident =
+          pexp_ident
+            ~loc:Location.none
+            Location.{ txt = Lident "__ppx_regex_reserved_label__"; loc = Location.none } in
+        pexp_apply
+          ~loc:ppat_loc
+          [%expr Re_str.string_match]
+          [nl regex; nl ident; nl [%expr 0]] in
       { pc_lhs = { ppat_desc = Ppat_any; ppat_loc; ppat_attributes = [] };
         pc_guard = Some guard;
         pc_rhs}
@@ -31,8 +42,14 @@ let regex_mapper argv =
   let expr mapper expr =
     match expr with
     | { pexp_desc = Pexp_extension ({ txt = "regex"; _ }, PStr [{ pstr_desc = Pstr_eval ({ pexp_desc = Pexp_match (e, cases) }, attrs) }]); pexp_loc; _ } ->
-      let new_cases = accept_cases e cases in
-      pexp_match ~loc:pexp_loc e new_cases
+      let new_cases = accept_cases cases in
+      let match_expr = 
+        pexp_match ~loc:pexp_loc [%expr __ppx_regex_reserved_label__] new_cases in
+      pexp_let
+        ~loc:Location.none
+        Nonrecursive
+        [value_binding ~loc:Location.none ~pat:[%pat? __ppx_regex_reserved_label__] ~expr:e]
+        match_expr
     | _ -> default_mapper.expr mapper expr in
   { default_mapper with expr }
 
